a924e7e7d6cb417ea763b3b7ce540b1f26266e85
YiJiuChow <19550518zrf@gmail.com>
2022-11-17 22:21:09 +0800

    Eureka 注册中心, Ribbon 拦截器负载均衡

---
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,36 @@
+package cn.itcast.order;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+
+    /**
+     * 创建RestTemplate到容器
+     * @return
+     */
+    @Bean
+    @LoadBalanced //Ribbon负载均衡
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+//
+//    @Bean
+//    public IRule randomRule() {
+//        return new RandomRule();
+//    }
+}
---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,30 @@
+package cn.itcast.order.service;
+
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Service
+public class OrderService {
+
+    @Resource
+    private OrderMapper orderMapper;
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        //String url = "http://101.35.240.17:8091/user/" + order.getUserId();
+        String url = "http://userservice/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+}
