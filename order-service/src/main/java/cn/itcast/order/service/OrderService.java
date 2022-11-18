a1fc1b29e40fb708b849a9c09980090d0ba86cb8
YiJiuChow <19550518zrf@gmail.com>
2022-11-18 21:02:36 +0800

    ......

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -20,7 +20,7 @@ public class OrderService {
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        //String url = "http://101.35.240.17:8091/user/" + order.getUserId();
+        //String url = "http://localhost:8091/user/" + order.getUserId();
         String url = "http://userservice/user/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         order.setUser(user);
