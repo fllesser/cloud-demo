a1938d210df1d4067b2683aa3a8a3f8869121e74
YiJiuChow <19550518zrf@gmail.com>
2022-11-20 17:10:45 +0800

    pom 依赖调整, 测试orderservice不到入feign依赖, 配置文件的连接池能否生效

--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -2,6 +2,7 @@ package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.service.OrderService;
+import org.springframework.cloud.openfeign.support.FeignHttpClientProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +14,17 @@ import javax.annotation.Resource;
 @RequestMapping("order")
 public class OrderController {
 
-   @Resource
-   private OrderService orderService;
+/*  测试
+    @Resource
+    private FeignHttpClientProperties feignHttpClientProperties;
+
+    @GetMapping("/test")
+    public FeignHttpClientProperties test() {
+        return feignHttpClientProperties;
+    }*/
+
+    @Resource
+    private OrderService orderService;
 
     @GetMapping("{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
